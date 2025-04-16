4963cdb69117385c9ee1030f8400ac27afccf5f8
Patrick Nindel <ufrpk@student.kit.edu>
2025-04-16 22:57:28 +0200

    Added Test for libcubwt

--- libcubwt-1.0.0/main.cu
+++ libcubwt-1.0.0/main.cu
@@ -14,9 +14,18 @@ int main()
     int64_t a = libcubwt_allocate_device_storage(&deviceStorage, 20);
     if (a == LIBCUBWT_NO_ERROR)
     {
-        const uint8_t *s = "yabbadabbado";
-        uint32_t *isa;
-        libcubwt_isa(deviceStorage, s, isa, 12);
+        char text[] = "yabbadabbado";
+        uint64_t len = strlen(text);
+
+        uint8_t bytes[12]; // oder malloc, wenn dynamisch
+
+        // Kopieren
+        for (size_t i = 0; i < len; i++)
+        {
+            bytes[i] = (uint8_t)text[i];
+        }
+        uint32_t *isa = nullptr;
+        libcubwt_isa(deviceStorage, bytes, isa, len);
         for (int i = 0; i < 12; i++)
         {
             printf("ISA: %u", *isa++);
