0b19720a2442f270eff402ef77502c70da598f5d
ufrpk <patricknindel@gmail.com>
2025-05-01 14:00:20 +0200

    Changed shell script

--- libcubBenchmark.sh
+++ libcubBenchmark.sh
@@ -2,11 +2,13 @@
 #SBATCH -J Test
 #SBATCH --partition=feigenbaum
 #SBATCH --get-user-env
-#SBATCH --time=5
+#SBATCH --time=20
 
 spack env activate nvidia
 
 cd libcubwt-1.0.0
-
-nvcc main.cu libcubwt.cu -o Test
-./Test input output
+mkdir build
+cd build
+cmake ..
+make -j8
+./BenchmarkLibcub input output
