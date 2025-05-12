474f18c6d28d9978bae7ae9a2dcb85fab2105c7b
ufrpk <patricknindel@gmail.com>
2025-05-12 18:25:46 +0200

    Changed shell script

--- multi-gpu-suffix-array/multigputest.sh
+++ multi-gpu-suffix-array/multigputest.sh
@@ -4,7 +4,11 @@
 #SBATCH --get-user-env
 #SBATCH --time=10
 #SBATCH --nodes="1"
-#SBATCH --mem="10240"
+#SBATCH --mem="4096"
+#SBATCH --gres=gpu:4
+
+module load compiler/gnu/12
 
 cd build 
-./suffix_array output.json dblp.xml dna english pitches proteins sources
+./suffix_array output.json dblp.xml dna pitches sources
+python ../../TestFramework/plot.py output.json
