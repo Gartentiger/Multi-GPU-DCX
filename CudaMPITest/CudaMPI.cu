8f3f2a35bc6e342e2cc7891b8435a612ef6b3b31
Patrick Nindel <ufrpk@student.kit.edu>
2025-05-26 16:47:51 +0200

    Test MPI

--- CudaMPITest/CudaMPI.cu
+++ CudaMPITest/CudaMPI.cu
@@ -5,7 +5,7 @@
 #include "cuda.h"
 #include "cuda_runtime.h"
 
-int main(int argc, char** argv)
+int main(int argc, char **argv)
 {
     MPI_Init(&argc, &argv);
 
@@ -16,35 +16,36 @@ int main(int argc, char** argv)
     // Get the rank of the calling process in the communicator MPI_COMM_WORLD
     int world_rank;
     MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
-    printf("* WorldSize: %d, Rank: \n", world_size, world_rank);
-    //std::vector<int> input2(2u * comm.size(), comm.rank_signed());
-    //std::vector<int> output = comm.alltoall(send_buf(input2));
-    //printf("Rank: %d, Size: %d, SRank: %d\n", comm.rank(), output[0], output[1]);
+    printf("* WorldSize: %d, Rank: %d\n", world_size, world_rank);
+    // std::vector<int> input2(2u * comm.size(), comm.rank_signed());
+    // std::vector<int> output = comm.alltoall(send_buf(input2));
+    // printf("Rank: %d, Size: %d, SRank: %d\n", comm.rank(), output[0], output[1]);
     printf("* Allocate memory [%d],GPU\n", world_rank);
-    double* d_a;
-    if (cudaMalloc((void**)&d_a, 1000 * sizeof(double)) != cudaSuccess) {
-        //errx(1, "cudaMalloc d_a[] failed");
-        printf("Error malloc", world_rank);
+    int *d_a;
+    if (cudaMalloc((void **)&d_a, 1000 * sizeof(int)) != cudaSuccess)
+    {
+        // errx(1, "cudaMalloc d_a[] failed");
+        cudaMemset(d_a, 0, 1000 * sizeof(int));
+        printf("Error malloc %d\n", world_rank);
         return 1;
     }
-    double* d_b = (double*)malloc(sizeof(double) * 1000);
-    printf("* Allocate memory [%d], CPU\n", world_rank);
-    if (!d_b) {
-        printf("Error malloc CPU\n");
-        return 1;
-    }
-    int err = 0; MPI_Status status;
+    int err = 0;
+    MPI_Status status;
     // From [1],GPU to [0],GPU
-    if (world_rank == 1) {
-        printf("* Send from [%d],GPU\n", world_rank);
-        err = MPI_Send(d_a, 1000, MPI_DOUBLE, 0, 2, MPI_COMM_WORLD);
+    if (world_rank == 1)
+    {
+        cudaMemset(d_a, 1, 1000 * sizeof(int));
+        err = MPI_Send(d_a, 1000, MPI_INT, 0, 2, MPI_COMM_WORLD);
+        printf("* Send from [%d],GPU Data %d\n", world_rank, d_a[0]);
     }
-    else if (world_rank == 0) {
-        printf("* Receive to [%d],GPU\n", world_rank);
-        err = MPI_Recv(d_a, 1000, MPI_DOUBLE, 1, 2, MPI_COMM_WORLD, &status);
+    else if (world_rank == 0)
+    {
+        err = MPI_Recv(d_a, 1000, MPI_INT, 1, 2, MPI_COMM_WORLD, &status);
+        printf("* Receive to [%d],GPU Data %d\n", world_rank, d_a[0]);
     }
-    if (err != MPI_SUCCESS) {
-        //errx(2, "MPI transport from [1],GPU to [0],GPU failed");
+    if (err != MPI_SUCCESS)
+    {
+        // errx(2, "MPI transport from [1],GPU to [0],GPU failed");
         printf("Error transport");
         return 1;
     }
