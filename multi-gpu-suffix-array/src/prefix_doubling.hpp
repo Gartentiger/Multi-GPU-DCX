f3e7d1c5aadf0e0212757bd4ba0f5a987cdaeaba
ufrpk <patricknindel@gmail.com>
2025-06-22 01:04:10 +0200

    Sanity Checks

--- multi-gpu-suffix-array/src/prefix_doubling.hpp
+++ multi-gpu-suffix-array/src/prefix_doubling.hpp
@@ -767,6 +767,7 @@ private:
             // Recover lens from above
             gpu.old_rank_start = *(mhost_temp_mem + 2 * NUM_GPUS + gpu_index);
             gpu.old_rank_end = *(mhost_temp_mem + 3 * NUM_GPUS + gpu_index);
+            printf("[%lu] old rank start %u, old rank end: %u\n", gpu_index, gpu.old_rank_start, gpu.old_rank_end);
             if (gpu.working_len > 0)
             {
                 cudaSetDevice(mcontext.get_device_id(gpu_index));
