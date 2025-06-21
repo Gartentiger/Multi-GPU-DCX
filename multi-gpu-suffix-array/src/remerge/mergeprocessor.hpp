640559b5bf42b780eff12d74b1a7baddd8c1f00a
ufrpk <patricknindel@gmail.com>
2025-06-21 12:15:44 +0200

    Sanity Checks

--- multi-gpu-suffix-array/src/remerge/mergeprocessor.hpp
+++ multi-gpu-suffix-array/src/remerge/mergeprocessor.hpp
@@ -139,7 +139,6 @@ namespace crossGPUReMerge {
                     printArrays << <1, 1, 0, mcontext.get_gpu_default_stream(node.info.index) >> > (node.info.keys, node.info.num_elements, (size_t)node.info.index, 1);
                 }
                 mcontext.sync_all_streams();
-                comm_world().barrier();
             }
 
             for (MergeNode& node : mnodes) {
