62c3eaf86d11b07015f715bfe44296fdf1a797a9
ufrpk <patricknindel@gmail.com>
2025-06-21 12:05:14 +0200

    Multi Node

--- multi-gpu-suffix-array/src/remerge/mergeprocessor.hpp
+++ multi-gpu-suffix-array/src/remerge/mergeprocessor.hpp
@@ -168,7 +168,7 @@ namespace crossGPUReMerge
             for (MergeNode node : mnodes) {
                 if (node.scheduled_work.searches.size() > 0) {
 
-                    printArrays << <1, 1, 0, mcontext.get_gpu_default_stream() >> > (node.info.keys, node.info.num_elements, (size_t)node.info.index, 1);
+                    printArrays << <1, 1, 0, mcontext.get_gpu_default_stream(node.info.index) >> > (node.info.keys, node.info.num_elements, (size_t)node.info.index, 1);
                 }
                 mcontext.sync_all_streams();
                 comm_world().barrier();
