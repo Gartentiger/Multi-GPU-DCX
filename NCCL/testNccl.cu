33d5b8378e9cee4dce2f7710049d1e80c6e7cce4
ufrpk <patricknindel@gmail.com>
2025-07-04 16:28:04 +0200

    Test MPI and NCCL

--- NCCL/testNccl.cu
+++ NCCL/testNccl.cu
@@ -51,8 +51,11 @@ int main(int argc, char** argv)
     using namespace kamping;
     kamping::Environment e;
     Communicator comm;
-
-    cudaSetDevice(world_rank());
+    int deviceCount;
+    CUDACHECK(cudaGetDeviceCount(&deviceCount));
+    int deviceId = world_rank() % deviceCount;
+    std::cout << "Device Id: " << deviceId << std::endl;
+    CUDACHECK(cudaSetDevice(deviceId));
 #ifdef USE_NCCL
     ncclComm_t nccl_comm;
     ncclUniqueId Id;
@@ -71,83 +74,182 @@ int main(int argc, char** argv)
     cudaStream_t stream;
     CUDACHECK(cudaStreamCreate(&stream));
 
-    if (world_rank() == 0) {
-        int canAccess;
-        cudaDeviceCanAccessPeer(&canAccess, world_rank(), 1);
-        if (canAccess) {
-            CUDACHECK(cudaDeviceEnablePeerAccess(1, 0));
-            printf("[%lu] peer to peer enabled", world_rank());
+    if (deviceCount > 1) {
+        if (world_rank() == 0) {
+            int canAccess;
+            cudaDeviceCanAccessPeer(&canAccess, world_rank(), 1);
+            if (canAccess) {
+                CUDACHECK(cudaDeviceEnablePeerAccess(1, 0));
+                printf("[%lu] peer to peer enabled\n", world_rank());
+            }
+
         }
-
-    }
-    else if (world_rank() == 1) {
-        int canAccess;
-        cudaDeviceCanAccessPeer(&canAccess, world_rank(), 0);
-        if (canAccess) {
-            CUDACHECK(cudaDeviceEnablePeerAccess(0, 0));
-            printf("[%lu] peer to peer enabled", world_rank());
+        else if (world_rank() == 1) {
+            int canAccess;
+            cudaDeviceCanAccessPeer(&canAccess, world_rank(), 0);
+            if (canAccess) {
+                CUDACHECK(cudaDeviceEnablePeerAccess(0, 0));
+                printf("[%lu] peer to peer enabled\n", world_rank());
+            }
         }
     }
 
 
-    int* sendBuf, * recvBuf;
-    cudaMalloc(&sendBuf, sizeof(int) * SEND_SIZE);
-    cudaMalloc(&recvBuf, sizeof(int) * SEND_SIZE);
 
-    std::vector<int> inp;
-    inp.reserve(SEND_SIZE);
-    std::srand(std::time(0));
-    for (int i = 0; i < SEND_SIZE; i++) {
-        inp[i] = std::rand();
-    }
-
-    cudaMemcpy(sendBuf, inp.data(), sizeof(int) * SEND_SIZE, cudaMemcpyHostToDevice);
-    std::span<int> sb(sendBuf, SEND_SIZE);
-    std::span<int> rb(recvBuf, SEND_SIZE);
-
-    auto& t = kamping::measurements::timer();
-    t.synchronize_and_start("pingping");
+    //auto& t = kamping::measurements::timer();
+    //t.synchronize_and_start("pingping");
 #ifdef USE_NCCL
-    for (int i = 0; i < SEND_TIMES; i++) {
-        t.synchronize_and_start("ping" + std::to_string(i));
-        ncclGroupStart();
-        if (world_rank() == 0) {
-            ncclSend(sendBuf, SEND_SIZE, ncclInt, 1, nccl_comm, stream);
-            ncclRecv(recvBuf, SEND_SIZE, ncclInt, 0, nccl_comm, stream);
-        }
-        else if (world_rank() == 1) {
-            ncclSend(sendBuf, SEND_SIZE, ncclInt, 0, nccl_comm, stream);
-            ncclRecv(recvBuf, SEND_SIZE, ncclInt, 1, nccl_comm, stream);
-        }
-        ncclGroupEnd();
-        t.stop_and_append();
-    }
-#else 
 
-    RequestPool req;
-    for (int i = 0; i < SEND_TIMES; i++) {
-        t.synchronize_and_start("ping" + std::to_string(i));
-        if (world_rank() == 0) {
-            comm_world().isend(send_buf(sb), send_count(SEND_SIZE), destination(1), request(req.get_request()));
-            comm_world().irecv(recv_buf(rb), recv_count(SEND_SIZE), request(req.get_request()));
+    // Loop From https://github.com/olcf-tutorials/MPI_ping_pong/blob/master/cuda_staged/ping_pong_cuda_staged.cu
+    for (int i = 0; i <= 27; i++) {
+
+        long int N = 1 << i;
+
+        // Allocate memory for A on CPU
+        double* A = (double*)malloc(N * sizeof(double));
+
+        // Initialize all elements of A to random values
+        for (int i = 0; i < N; i++) {
+            A[i] = (double)rand() / (double)RAND_MAX;
         }
-        else if (world_rank() == 1) {
-            comm_world().isend(send_buf(sb), send_count(SEND_SIZE), destination(0), request(req.get_request()));
-            comm_world().irecv(recv_buf(rb), recv_count(SEND_SIZE), request(req.get_request()));
+
+        double* d_A;
+        CUDACHECK(cudaMalloc(&d_A, N * sizeof(double)));
+        CUDACHECK(cudaMemcpy(d_A, A, N * sizeof(double), cudaMemcpyHostToDevice));
+
+        int tag1 = 10;
+        int tag2 = 20;
+
+        int loop_count = 50;
+        // Warm-up loop
+        for (int i = 1; i <= 5; i++) {
+            if (world_rank() == 0) {
+                CUDACHECK(cudaMemcpy(A, d_A, N * sizeof(double), cudaMemcpyDeviceToHost));
+                ncclSend(A, N, ncclDouble, 1, nccl_comm, stream);
+                ncclRecv(A, N, ncclDouble, 1, nccl_comm, stream);
+                CUDACHECK(cudaMemcpy(d_A, A, N * sizeof(double), cudaMemcpyHostToDevice));
+            }
+            else if (world_rank() == 1) {
+                ncclRecv(A, N, ncclDouble, 0, nccl_comm, stream);
+                CUDACHECK(cudaMemcpy(d_A, A, N * sizeof(double), cudaMemcpyHostToDevice));
+                CUDACHECK(cudaMemcpy(A, d_A, N * sizeof(double), cudaMemcpyDeviceToHost));
+                ncclSend(A, N, ncclDouble, 0, nccl_comm, stream);
+            }
         }
-        req.wait_all();
-        t.stop_and_append();
+
+        // Time ping-pong for loop_count iterations of data transfer size 8*N bytes
+        double start_time, stop_time, elapsed_time;
+        start_time = MPI_Wtime();
+
+        for (int i = 1; i <= loop_count; i++) {
+            if (world_rank() == 0) {
+                CUDACHECK(cudaMemcpy(A, d_A, N * sizeof(double), cudaMemcpyDeviceToHost));
+                ncclSend(A, N, ncclDouble, 1, nccl_comm, stream);
+                ncclRecv(A, N, ncclDouble, 1, nccl_comm, stream);
+                CUDACHECK(cudaMemcpy(d_A, A, N * sizeof(double), cudaMemcpyHostToDevice));
+            }
+            else if (world_rank() == 1) {
+                ncclRecv(A, N, ncclDouble, 0, nccl_comm, stream);
+                CUDACHECK(cudaMemcpy(d_A, A, N * sizeof(double), cudaMemcpyHostToDevice));
+                CUDACHECK(cudaMemcpy(A, d_A, N * sizeof(double), cudaMemcpyDeviceToHost));
+                ncclSend(A, N, ncclDouble, 0, nccl_comm, stream);
+            }
+        }
+
+        stop_time = MPI_Wtime();
+        elapsed_time = stop_time - start_time;
+
+        long int num_B = 8 * N;
+        long int B_in_GB = 1 << 30;
+        double num_GB = (double)num_B / (double)B_in_GB;
+        double avg_time_per_transfer = elapsed_time / (2.0 * (double)loop_count);
+
+        if (world_rank() == 0) printf("Transfer size (B): %10li, Transfer Time (s): %15.9f, Bandwidth (GB/s): %15.9f\n", num_B, avg_time_per_transfer, num_GB / avg_time_per_transfer);
+
+        CUDACHECK(cudaFree(d_A));
+        free(A);
+    }
+
+#else 
+    // Loop From https://github.com/olcf-tutorials/MPI_ping_pong/blob/master/cuda_staged/ping_pong_cuda_staged.cu
+    for (int i = 0; i <= 27; i++) {
+
+        long int N = 1 << i;
+
+        // Allocate memory for A on CPU
+        double* A = (double*)malloc(N * sizeof(double));
+
+        // Initialize all elements of A to random values
+        for (int i = 0; i < N; i++) {
+            A[i] = (double)rand() / (double)RAND_MAX;
+        }
+
+        double* d_A;
+        CUDACHECK(cudaMalloc(&d_A, N * sizeof(double)));
+        CUDACHECK(cudaMemcpy(d_A, A, N * sizeof(double), cudaMemcpyHostToDevice));
+
+        int tag1 = 10;
+        int tag2 = 20;
+
+        int loop_count = 50;
+        MPI_Status stat;
+        // Warm-up loop
+        for (int i = 1; i <= 5; i++) {
+            if (world_rank() == 0) {
+                CUDACHECK(cudaMemcpy(A, d_A, N * sizeof(double), cudaMemcpyDeviceToHost));
+                MPI_Send(A, N, MPI_DOUBLE, 1, tag1, MPI_COMM_WORLD);
+                MPI_Recv(A, N, MPI_DOUBLE, 1, tag2, MPI_COMM_WORLD, &stat);
+                CUDACHECK(cudaMemcpy(d_A, A, N * sizeof(double), cudaMemcpyHostToDevice));
+            }
+            else if (world_rank() == 1) {
+                MPI_Recv(A, N, MPI_DOUBLE, 0, tag1, MPI_COMM_WORLD, &stat);
+                CUDACHECK(cudaMemcpy(d_A, A, N * sizeof(double), cudaMemcpyHostToDevice));
+                CUDACHECK(cudaMemcpy(A, d_A, N * sizeof(double), cudaMemcpyDeviceToHost));
+                MPI_Send(A, N, MPI_DOUBLE, 0, tag2, MPI_COMM_WORLD);
+            }
+        }
+
+        // Time ping-pong for loop_count iterations of data transfer size 8*N bytes
+        double start_time, stop_time, elapsed_time;
+        start_time = MPI_Wtime();
+
+        for (int i = 1; i <= loop_count; i++) {
+            if (world_rank() == 0) {
+                CUDACHECK(cudaMemcpy(A, d_A, N * sizeof(double), cudaMemcpyDeviceToHost));
+                MPI_Send(A, N, MPI_DOUBLE, 1, tag1, MPI_COMM_WORLD);
+                MPI_Recv(A, N, MPI_DOUBLE, 1, tag2, MPI_COMM_WORLD, &stat);
+                CUDACHECK(cudaMemcpy(d_A, A, N * sizeof(double), cudaMemcpyHostToDevice));
+            }
+            else if (world_rank() == 1) {
+                MPI_Recv(A, N, MPI_DOUBLE, 0, tag1, MPI_COMM_WORLD, &stat);
+                CUDACHECK(cudaMemcpy(d_A, A, N * sizeof(double), cudaMemcpyHostToDevice));
+                CUDACHECK(cudaMemcpy(A, d_A, N * sizeof(double), cudaMemcpyDeviceToHost));
+                MPI_Send(A, N, MPI_DOUBLE, 0, tag2, MPI_COMM_WORLD);
+            }
+        }
+
+        stop_time = MPI_Wtime();
+        elapsed_time = stop_time - start_time;
+
+        long int num_B = 8 * N;
+        long int B_in_GB = 1 << 30;
+        double num_GB = (double)num_B / (double)B_in_GB;
+        double avg_time_per_transfer = elapsed_time / (2.0 * (double)loop_count);
+
+        if (world_rank() == 0) printf("Transfer size (B): %10li, Transfer Time (s): %15.9f, Bandwidth (GB/s): %15.9f\n", num_B, avg_time_per_transfer, num_GB / avg_time_per_transfer);
+
+        CUDACHECK(cudaFree(d_A));
+        free(A);
     }
 #endif
-    t.stop();
 
 
-    std::cout << "Ping pong complete" << std::endl;
-    std::ofstream outFile(argv[1], std::ios::app);
-    t.aggregate_and_print(
-        kamping::measurements::SimpleJsonPrinter{ outFile, {} });
-    std::cout << std::endl;
-    t.aggregate_and_print(kamping::measurements::FlatPrinter{});
-    std::cout << std::endl;
+    // std::cout << "[" << world_rank() << "]" << "Ping pong complete" << std::endl;
+    // std::ofstream outFile(argv[1], std::ios::app);
+    // t.aggregate_and_print(
+    //     kamping::measurements::SimpleJsonPrinter{ outFile, {} });
+    // std::cout << std::endl;
+    // t.aggregate_and_print(kamping::measurements::FlatPrinter{});
+    // std::cout << std::endl;
     return 0;
 }
