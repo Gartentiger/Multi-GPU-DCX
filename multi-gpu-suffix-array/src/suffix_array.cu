298c97079397cc988cb4e480d1bb1ef84564bda2
ufrpk <patricknindel@gmail.com>
2025-07-14 12:32:06 +0200

    Revert some changes

--- multi-gpu-suffix-array/src/suffix_array.cu
+++ multi-gpu-suffix-array/src/suffix_array.cu
@@ -821,8 +821,8 @@ int main(int argc, char** argv)
 
     MultiGPUContext<NUM_GPUS> context(&gpu_ids);
 #else 
-    const std::array<uint, NUM_GPUS> gpu_ids{ 0,0,0,0 };
-    MultiGPUContext<NUM_GPUS> context(&gpu_ids);
+    const std::array<uint, NUM_GPUS> gpu_ids;
+    MultiGPUContext<NUM_GPUS> context;
 #endif
     SuffixSorter sorter(context, realLen, input);
     sorter.alloc();
