50c944a642737e965db8dcc10ce79068542678da
ufrpk <patricknindel@gmail.com>
2025-07-23 14:21:21 +0200

    Multi Node

--- multi-gpu-suffix-array/src/suffix_array.cu
+++ multi-gpu-suffix-array/src/suffix_array.cu
@@ -783,14 +783,14 @@ private:
         CUERR;
         mcontext.sync_gpu_default_stream(gpu_index);
 
-        // MPI_File outputFile;
-        // MPI_File_open(MPI_COMM_WORLD, "outputData",
-        //     MPI_MODE_CREATE | MPI_MODE_WRONLY,
-        //     MPI_INFO_NULL, &outputFile);
-        // MPI_File_write_at_all(outputFile, gpu.offset, h_result, gpu.num_elements, MPI_UINT32_T, MPI_STATUS_IGNORE);
-        // // MPI_File_write_at(outputFile, gpu.offset, h_result, gpu.num_elements, MPI_UINT32_T, MPI_STATUS_IGNORE);
+        MPI_File outputFile;
+        MPI_File_open(MPI_COMM_WORLD, "outputData",
+            MPI_MODE_CREATE | MPI_MODE_WRONLY,
+            MPI_INFO_NULL, &outputFile);
+        MPI_File_write_at_all(outputFile, gpu.offset, h_result, gpu.num_elements, MPI_UINT32_T, MPI_STATUS_IGNORE);
+        // MPI_File_write_at(outputFile, gpu.offset, h_result, gpu.num_elements, MPI_UINT32_T, MPI_STATUS_IGNORE);
 
-        // MPI_File_close(&outputFile);
+        MPI_File_close(&outputFile);
 
         //}
         //mcontext.sync_default_streams();
