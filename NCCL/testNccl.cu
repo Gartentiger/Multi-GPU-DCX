54f546188af08a3417b57ced8f66418b4f1fc375
ufrpk <patricknindel@gmail.com>
2025-09-01 14:59:00 +0200

    Multi Node

--- NCCL/testNccl.cu
+++ NCCL/testNccl.cu
@@ -63,7 +63,6 @@ int main(int argc, char** argv)
     CUDACHECK(cudaSetDevice(0));
 
 
-#ifdef USE_NCCL
     //ncclComm_t nccl_comm;
     //ncclUniqueId Id;
     // if (world_rank() == 0) {
@@ -77,7 +76,6 @@ int main(int argc, char** argv)
 
     //NCCLCHECK(ncclCommInitRank(&nccl_comm, world_size(), Id, world_rank()));
 
-#endif
     cudaStream_t stream;
     CUDACHECK(cudaStreamCreate(&stream));
     CUDACHECK(cudaSetDevice(1));
@@ -111,7 +109,6 @@ int main(int argc, char** argv)
 
     //auto& t = kamping::measurements::timer();
     //t.synchronize_and_start("pingping");
-#ifdef USE_NCCL
 
     // Loop From https://github.com/olcf-tutorials/MPI_ping_pong/blob/master/cuda_staged/ping_pong_cuda_staged.cu
     for (int i = 0; i <= 27; i++) {
@@ -202,69 +199,68 @@ int main(int argc, char** argv)
         free(B);
     }
 
-#else 
-    for (int i = 0; i <= 27; i++) {
+    // for (int i = 0; i <= 27; i++) {
 
-        long int N = 1 << i;
+    //     long int N = 1 << i;
 
-        // Allocate memory for A on CPU
-        double* A = (double*)malloc(N * sizeof(double));
+    //     // Allocate memory for A on CPU
+    //     double* A = (double*)malloc(N * sizeof(double));
 
-        // Initialize all elements of A to random values
-        for (int i = 0; i < N; i++) {
-            A[i] = (double)rand() / (double)RAND_MAX;
-        }
+    //     // Initialize all elements of A to random values
+    //     for (int i = 0; i < N; i++) {
+    //         A[i] = (double)rand() / (double)RAND_MAX;
+    //     }
 
-        double* d_A;
-        CUDACHECK(cudaMalloc(&d_A, N * sizeof(double)));
-        CUDACHECK(cudaMemcpy(d_A, A, N * sizeof(double), cudaMemcpyHostToDevice));
+    //     double* d_A;
+    //     CUDACHECK(cudaMalloc(&d_A, N * sizeof(double)));
+    //     CUDACHECK(cudaMemcpy(d_A, A, N * sizeof(double), cudaMemcpyHostToDevice));
 
-        int tag1 = 10;
-        int tag2 = 20;
+    //     int tag1 = 10;
+    //     int tag2 = 20;
 
-        int loop_count = 50;
+    //     int loop_count = 50;
 
-        // Warm-up loop
-        for (int i = 1; i <= 5; i++) {
-            if (world_rank() == 0) {
-                comm_world().send(send_buf(std::span<double>(d_A, N)), send_count(N), destination(1), tag(tag1));
-                comm_world().recv(recv_buf(std::span<double>(d_A, N)), recv_count(N), source(1), tag(tag2));
-            }
-            else if (world_rank() == 1) {
-                comm_world().recv(recv_buf(std::span<double>(d_A, N)), recv_count(N), source(0), tag(tag1));
-                comm_world().send(send_buf(std::span<double>(d_A, N)), send_count(N), destination(0), tag(tag2));
-            }
-        }
+    //     // Warm-up loop
+    //     for (int i = 1; i <= 5; i++) {
+    //         if (world_rank() == 0) {
+    //             comm_world().send(send_buf(std::span<double>(d_A, N)), send_count(N), destination(1), tag(tag1));
+    //             comm_world().recv(recv_buf(std::span<double>(d_A, N)), recv_count(N), source(1), tag(tag2));
+    //         }
+    //         else if (world_rank() == 1) {
+    //             comm_world().recv(recv_buf(std::span<double>(d_A, N)), recv_count(N), source(0), tag(tag1));
+    //             comm_world().send(send_buf(std::span<double>(d_A, N)), send_count(N), destination(0), tag(tag2));
+    //         }
+    //     }
 
-        // Time ping-pong for loop_count iterations of data transfer size 8*N bytes
-        double start_time, stop_time, elapsed_time;
-        start_time = MPI_Wtime();
-        RequestPool pool;
-        for (int i = 1; i <= loop_count; i++) {
-            if (world_rank() == 0) {
-                comm_world().isend(send_buf(std::span<double>(d_A, N)), send_count(N), destination(1), tag(tag1), request(pool.get_request()));
-                comm_world().irecv(recv_buf(std::span<double>(d_A, N)), recv_count(N), source(1), tag(tag2), request(pool.get_request()));
-            }
-            else if (world_rank() == 1) {
-                comm_world().isend(send_buf(std::span<double>(d_A, N)), send_count(N), destination(0), tag(tag2), request(pool.get_request()));
-                comm_world().irecv(recv_buf(std::span<double>(d_A, N)), recv_count(N), source(0), tag(tag1), request(pool.get_request()));
-            }
-        }
-        pool.wait_all();
-        stop_time = MPI_Wtime();
-        elapsed_time = stop_time - start_time;
+    //     // Time ping-pong for loop_count iterations of data transfer size 8*N bytes
+    //     double start_time, stop_time, elapsed_time;
+    //     start_time = MPI_Wtime();
+    //     RequestPool pool;
+    //     for (int i = 1; i <= loop_count; i++) {
+    //         if (world_rank() == 0) {
+    //             comm_world().isend(send_buf(std::span<double>(d_A, N)), send_count(N), destination(1), tag(tag1), request(pool.get_request()));
+    //             comm_world().irecv(recv_buf(std::span<double>(d_A, N)), recv_count(N), source(1), tag(tag2), request(pool.get_request()));
+    //         }
+    //         else if (world_rank() == 1) {
+    //             comm_world().isend(send_buf(std::span<double>(d_A, N)), send_count(N), destination(0), tag(tag2), request(pool.get_request()));
+    //             comm_world().irecv(recv_buf(std::span<double>(d_A, N)), recv_count(N), source(0), tag(tag1), request(pool.get_request()));
+    //         }
+    //     }
+    //     pool.wait_all();
+    //     stop_time = MPI_Wtime();
+    //     elapsed_time = stop_time - start_time;
 
-        long int num_B = 8 * N;
-        long int B_in_GB = 1 << 30;
-        double num_GB = (double)num_B / (double)B_in_GB;
-        double avg_time_per_transfer = elapsed_time / (2.0 * (double)loop_count);
+    //     long int num_B = 8 * N;
+    //     long int B_in_GB = 1 << 30;
+    //     double num_GB = (double)num_B / (double)B_in_GB;
+    //     double avg_time_per_transfer = elapsed_time / (2.0 * (double)loop_count);
 
-        if (world_rank() == 0) printf("Transfer size (B): %10li, Transfer Time (s): %15.9f, Bandwidth (GB/s): %15.9f\n", num_B, avg_time_per_transfer, num_GB / avg_time_per_transfer);
+    //     if (world_rank() == 0) printf("Transfer size (B): %10li, Transfer Time (s): %15.9f, Bandwidth (GB/s): %15.9f\n", num_B, avg_time_per_transfer, num_GB / avg_time_per_transfer);
 
-        CUDACHECK(cudaFree(d_A));
-        free(A);
-    }
-#endif
+    //     CUDACHECK(cudaFree(d_A));
+    //     free(A);
+    // }
+
 
 
     // std::cout << "[" << world_rank() << "]" << "Ping pong complete" << std::endl;
