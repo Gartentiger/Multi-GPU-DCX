2fcff4eb68a5b7fbe242f342ff6cb2ff5edb3c1e
ufrpk <patricknindel@gmail.com>
2025-09-11 01:03:19 +0200

    debug

--- multi-gpu-suffix-array/src/gossip/all_to_all.cuh
+++ multi-gpu-suffix-array/src/gossip/all_to_all.cuh
@@ -58,10 +58,10 @@ namespace gossip {
                     const table_t src_index = h_table[src_gpu][dest_gpu];
                     const table_t dest_index = v_table[src_gpu][dest_gpu];
                     const table_t len = table[src_gpu][dest_gpu];
-                    if (world_rank() == 0) {
-                        printf("send to [%u] len: %u\n", dest_gpu, len);
-                    }
                     if (src_gpu == world_rank()) {
+                        if (world_rank() == 0) {
+                            printf("send to [%u] len: %u\n", dest_gpu, len);
+                        }
                         key_t* from_k = node_info[src_gpu].src_keys + src_index;
 
                         ncclSend(from_k, sizeof(key_t) * len, ncclChar, dest_gpu, nccl_comm, context.get_streams(src_gpu)[dest_gpu]);
