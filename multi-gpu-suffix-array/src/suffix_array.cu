60a6ba10af388fa691dc26ccf54e4baf7a7cd5a8
ufrpk <patricknindel@gmail.com>
2025-09-11 11:22:20 +0200

    debug

--- multi-gpu-suffix-array/src/suffix_array.cu
+++ multi-gpu-suffix-array/src/suffix_array.cu
@@ -986,7 +986,7 @@ void ncclMeasure(MultiGPUContext<NUM_GPUS>& context)
     std::random_device rd;
     std::mt19937 g(rd());
     std::uniform_int_distribution<std::mt19937::result_type> randomDist(0, UINT32_MAX);
-    const int rounds = 26;
+    const int rounds = 25;
     std::array<double, rounds> alg_bandwidth;
     ncclComm_t nccl_comm = context.get_nccl();
     const int start_offset = 0;
@@ -1097,7 +1097,7 @@ void alltoallMeasure(MultiGPUContext<NUM_GPUS>& context)
     std::random_device rd;
     std::mt19937 g(rd());
     std::uniform_int_distribution<std::mt19937::result_type> randomDist(0, UINT32_MAX);
-    const int rounds = 26;
+    const int rounds = 25;
     const int start_offset = 0;
     std::array<double, rounds> alg_bandwidth;
     for (int i = start_offset; i <= rounds; i++)
