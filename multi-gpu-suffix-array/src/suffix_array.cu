55d634ef9a34e2721441e3dfcbf0bd7b7b362a9f
ufrpk <patricknindel@gmail.com>
2025-09-25 11:19:08 +0200

    debug

--- multi-gpu-suffix-array/src/suffix_array.cu
+++ multi-gpu-suffix-array/src/suffix_array.cu
@@ -173,7 +173,7 @@ __global__ void printArrayss(uint64_t* input, size_t size, size_t rank)
 {
     for (size_t i = 0; i < size; i++) {
 
-        printf("[%lu] data: %lu\n", rank, input[i]);
+        printf("[%lu] data[%lu]: %lu\n", rank, i, input[i]);
     }
     printf("---------------------------------------------------------------------------\n");
 }
@@ -1911,7 +1911,7 @@ int main(int argc, char** argv)
     std::mt19937 g(rd());
     std::uniform_int_distribution<std::mt19937::result_type> randomDistChar(0, 255);
     std::uniform_int_distribution<std::mt19937::result_type> randomDistUint(0, UINT32_MAX);
-    std::uniform_int_distribution<std::mt19937::result_type> randomDistSize(0, UINT64_MAX);
+    std::uniform_int_distribution<std::mt19937::result_type> randomDistSize(0, 599);
     using T = uint64_t;
     for (size_t round = 0; round < 1; round++)
     {
