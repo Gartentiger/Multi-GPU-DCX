2c5f01902318b1b56efcb03a85f6c0062042b3c2
ufrpk <patricknindel@gmail.com>
2025-10-16 14:41:49 +0200

    debug

--- multi-gpu-suffix-array/src/prefix_doubling.hpp
+++ multi-gpu-suffix-array/src/prefix_doubling.hpp
@@ -718,6 +718,7 @@ private:
             in_buffer[world_rank()] = keys.Current() == gpu.Kmer_buffer ? true : false;
 
             printf("[%lu] local_in_buffer: %s\n", world_rank(), keys.Current() == gpu.Kmer_buffer ? "true" : "false");
+            ASSERT(keys.Current() == gpu.Kmer_buffer ? values.Current() == gpu.Sa_index : values.Current() == gpu.Isa);
         }
         // only for in node merges
 
@@ -734,10 +735,10 @@ private:
             mcontext.get_device_temp_allocator(gpu_index).init(gpu.Kmer_temp1, mreserved_len * 3 * sizeof(sa_index_t));
         }
 
-        for (size_t i = 0; i < NUM_GPUS; i++)
-        {
-            printf("[%lu] in_buffer[%lu]: %s\n", world_rank(), i, in_buffer[i] ? "true" : "false");
-        }
+        // for (size_t i = 0; i < NUM_GPUS; i++)
+        // {
+        //     printf("[%lu] in_buffer[%lu]: %s\n", world_rank(), i, in_buffer[i] ? "true" : "false");
+        // }
 
         merge_manager.set_node_info(merge_nodes_info);
 
@@ -755,22 +756,22 @@ private:
         comm_world().barrier();
         // printf("after init merging\n");
 
-        std::vector<kmerDCX> localList(mgpus[world_rank()].working_len);
-        cudaMemcpy(localList.data(), in_buffer[world_rank()] ? mgpus[world_rank()].Kmer_buffer : mgpus[world_rank()].Kmer, sizeof(kmerDCX) * localList.size(), cudaMemcpyDeviceToHost);
-        std::vector<kmerDCX> sortedList = comm_world().gatherv(send_buf(localList), root(0));
-        if (world_rank() == 0) {
-            ASSERT(thrust::is_sorted(sortedList.begin(), sortedList.end(), KmerComparator{}));
-        }
-        comm_world().barrier();
-        printf("[%lu] after check\n", world_rank());
+        // std::vector<kmerDCX> localList(mgpus[world_rank()].working_len);
+        // cudaMemcpy(localList.data(), in_buffer[world_rank()] ? mgpus[world_rank()].Kmer_buffer : mgpus[world_rank()].Kmer, sizeof(kmerDCX) * localList.size(), cudaMemcpyDeviceToHost);
+        // std::vector<kmerDCX> sortedList = comm_world().gatherv(send_buf(localList), root(0));
+        // if (world_rank() == 0) {
+        //     ASSERT(thrust::is_sorted(sortedList.begin(), sortedList.end(), KmerComparator{}));
+        // }
+        // comm_world().barrier();
+        // printf("[%lu] after check\n", world_rank());
 
         // t.stop();
-        t.aggregate_and_print(
-            kamping::measurements::SimpleJsonPrinter{ std::cout }
-        );
-        std::cout << std::endl;
-        t.aggregate_and_print(kamping::measurements::FlatPrinter{});
-        std::cout << std::endl;
+        // t.aggregate_and_print(
+        //     kamping::measurements::SimpleJsonPrinter{ std::cout }
+        // );
+        // std::cout << std::endl;
+        // t.aggregate_and_print(kamping::measurements::FlatPrinter{});
+        // std::cout << std::endl;
         TIMER_STOP_MAIN_STAGE(MainStages::Initial_Merge);
 
     }
@@ -815,138 +816,131 @@ private:
         mcontext.sync_default_streams();
         mcontext.get_device_temp_allocator(gpu_index).reset();
         // printf("[%lu] after write ranks diff\n", world_rank());
-        if (world_rank() == 0) {
-            printArrayss << <1, 1 >> > (current_buffer, reinterpret_cast<sa_index_t*>(other_buffer), std::min(80UL, gpu.working_len), world_rank());
-            mcontext.sync_all_streams();
-        }
-        std::vector<kmer> kmerCheck(gpu.working_len);
-        cudaMemcpy(kmerCheck.data(), current_buffer, sizeof(kmer) * gpu.working_len, cudaMemcpyDeviceToHost);
-        auto allKmer = comm_world().gatherv(send_buf(kmerCheck), root(0));
-        comm_world().barrier();
+
+        // std::vector<kmer> kmerCheck(gpu.working_len);
+        // cudaMemcpy(kmerCheck.data(), current_buffer, sizeof(kmer) * gpu.working_len, cudaMemcpyDeviceToHost);
+        // auto allKmer = comm_world().gatherv(send_buf(kmerCheck), root(0));
+        // comm_world().barrier();
 
         // printArrayss << <1, 1 >> > (current_buffer, reinterpret_cast<sa_index_t*>(other_buffer), std::min(20UL, gpu.working_len), world_rank());
         // mcontext.sync_all_streams();
         // comm_world().barrier();
 
 
-        printf("[%lu] after check initial ranks\n", world_rank());
-        {
-            std::vector<sa_index_t> check(gpu.working_len);
-            cudaMemcpy(check.data(), reinterpret_cast<sa_index_t*>(other_buffer), sizeof(sa_index_t) * gpu.working_len, cudaMemcpyDeviceToHost);
-            std::vector<kmer> local_kmer(gpu.working_len);
-            cudaMemcpy(local_kmer.data(), current_buffer, sizeof(kmer) * gpu.working_len, cudaMemcpyDeviceToHost);
-            bool in_group = check[0] == 0;
-            for (size_t i = 0; i < check.size() - 1; i++)
-            {
-                if (check[i] != 0 && check[i + 1] == 0) {
-                    if (local_kmer[i] != local_kmer[i + 1]) {
-                        printf("[%lu] %lu and %lu are equal but next not starting with 0\n", world_rank(), i, i + 1);
-                    }
+        // printf("[%lu] after check initial ranks\n", world_rank());
+        // {
+        //     std::vector<sa_index_t> check(gpu.working_len);
+        //     cudaMemcpy(check.data(), reinterpret_cast<sa_index_t*>(other_buffer), sizeof(sa_index_t) * gpu.working_len, cudaMemcpyDeviceToHost);
+        //     std::vector<kmer> local_kmer(gpu.working_len);
+        //     cudaMemcpy(local_kmer.data(), current_buffer, sizeof(kmer) * gpu.working_len, cudaMemcpyDeviceToHost);
+        //     bool in_group = check[0] == 0;
+        //     for (size_t i = 0; i < check.size() - 1; i++)
+        //     {
+        //         if (check[i] != 0 && check[i + 1] == 0) {
+        //             if (local_kmer[i] != local_kmer[i + 1]) {
+        //                 printf("[%lu] %lu and %lu are equal but next not starting with 0\n", world_rank(), i, i + 1);
+        //             }
 
-                    ASSERT(local_kmer[i] == local_kmer[i + 1]);
-                    in_group = true;
-                }
-                else {
-                    if (check[i] == 0 && check[i + 1] == 0) {
-                        ASSERT(in_group);
-                        if (local_kmer[i] != local_kmer[i + 1]) {
-                            printf("%lu and %lu are not equal but have the same rank:\n", i, i + 1);
-                            for (size_t k = 0;k < DCX::X; k++)
-                            {
-                                printf("%c, ", local_kmer[i].kmer[k]);
-                            }
-                            printf("\n i+1\n");
-                            for (size_t k = 0;k < DCX::X; k++)
-                            {
-                                printf("%c, ", local_kmer[i + 1].kmer[k]);
-                            }
-                            printf("\n");
-                        }
-                        ASSERT(local_kmer[i] == local_kmer[i + 1]);
-                    }
-                    if (check[i] != 0 && check[i + 1] != 0) {
-                        if (check[i] + 1 != check[i + 1]) {
-                            printf("%lu and %lu are not equal but have the same rank:\n", i, i + 1);
-                            for (size_t k = 0;k < DCX::X; k++)
-                            {
-                                printf("%c, ", local_kmer[i].kmer[k]);
-                            }
-                            printf("\n i+1\n");
-                            for (size_t k = 0;k < DCX::X; k++)
-                            {
-                                printf("%c, ", local_kmer[i + 1].kmer[k]);
-                            }
-                            printf("\n");
-                        }
-                        ASSERT(check[i] + 1 == check[i + 1]);
-                        ASSERT(!in_group);
-                    }
-                    if (check[i] == 0 && check[i + 1] != 0) {
-                        ASSERT(in_group);
-                        if (local_kmer[i] == local_kmer[i + 1]) {
-                            printf("%lu and %lu are not equal but have the same rank:\n", i, i + 1);
-                            for (size_t k = 0;k < DCX::X; k++)
-                            {
-                                printf("%c, ", local_kmer[i].kmer[k]);
-                            }
-                            printf("\n i+1\n");
-                            for (size_t k = 0;k < DCX::X; k++)
-                            {
-                                printf("%c, ", local_kmer[i + 1].kmer[k]);
-                            }
-                            printf("\n");
-                        }
-                        ASSERT(local_kmer[i] != local_kmer[i + 1]);
-                        in_group = false;
-                    }
-                }
-            }
-            comm_world().barrier();
-        }
+        //             ASSERT(local_kmer[i] == local_kmer[i + 1]);
+        //             in_group = true;
+        //         }
+        //         else {
+        //             if (check[i] == 0 && check[i + 1] == 0) {
+        //                 ASSERT(in_group);
+        //                 if (local_kmer[i] != local_kmer[i + 1]) {
+        //                     printf("%lu and %lu are not equal but have the same rank:\n", i, i + 1);
+        //                     for (size_t k = 0;k < DCX::X; k++)
+        //                     {
+        //                         printf("%c, ", local_kmer[i].kmer[k]);
+        //                     }
+        //                     printf("\n i+1\n");
+        //                     for (size_t k = 0;k < DCX::X; k++)
+        //                     {
+        //                         printf("%c, ", local_kmer[i + 1].kmer[k]);
+        //                     }
+        //                     printf("\n");
+        //                 }
+        //                 ASSERT(local_kmer[i] == local_kmer[i + 1]);
+        //             }
+        //             if (check[i] != 0 && check[i + 1] != 0) {
+        //                 if (check[i] + 1 != check[i + 1]) {
+        //                     printf("%lu and %lu are not equal but have the same rank:\n", i, i + 1);
+        //                     for (size_t k = 0;k < DCX::X; k++)
+        //                     {
+        //                         printf("%c, ", local_kmer[i].kmer[k]);
+        //                     }
+        //                     printf("\n i+1\n");
+        //                     for (size_t k = 0;k < DCX::X; k++)
+        //                     {
+        //                         printf("%c, ", local_kmer[i + 1].kmer[k]);
+        //                     }
+        //                     printf("\n");
+        //                 }
+        //                 ASSERT(check[i] + 1 == check[i + 1]);
+        //                 ASSERT(!in_group);
+        //             }
+        //             if (check[i] == 0 && check[i + 1] != 0) {
+        //                 ASSERT(in_group);
+        //                 if (local_kmer[i] == local_kmer[i + 1]) {
+        //                     printf("%lu and %lu are not equal but have the same rank:\n", i, i + 1);
+        //                     for (size_t k = 0;k < DCX::X; k++)
+        //                     {
+        //                         printf("%c, ", local_kmer[i].kmer[k]);
+        //                     }
+        //                     printf("\n i+1\n");
+        //                     for (size_t k = 0;k < DCX::X; k++)
+        //                     {
+        //                         printf("%c, ", local_kmer[i + 1].kmer[k]);
+        //                     }
+        //                     printf("\n");
+        //                 }
+        //                 ASSERT(local_kmer[i] != local_kmer[i + 1]);
+        //                 in_group = false;
+        //             }
+        //         }
+        //     }
+        //     comm_world().barrier();
+        // }
 
         do_max_scan_on_ranks(true);
 
 
-        mcontext.sync_all_streams();
-        comm_world().barrier();
+        // mcontext.sync_all_streams();
+        // comm_world().barrier();
         // printf("[%lu] after do max\n", world_rank());
-        if (world_rank() == 0) {
-            printArrayss << <1, 1 >> > (gpu.Sa_rank, std::min(80UL, gpu.working_len), world_rank());
-        }
+        // printArrayss << <1, 1 >> > (current_buffer, gpu.Sa_rank, std::min(20UL, gpu.working_len), world_rank());
+        // mcontext.sync_default_streams();
+        // comm_world().barrier();
 
+        // std::vector<sa_index_t> sa(mgpus[world_rank()].working_len);
+        // cudaMemcpy(sa.data(), mgpus[world_rank()].Sa_rank, sizeof(sa_index_t) * mgpus[world_rank()].working_len, cudaMemcpyDeviceToHost);
 
-        mcontext.sync_default_streams();
-        comm_world().barrier();
+        // auto check = comm_world().gatherv(send_buf(sa), root(0));
+        // comm_world().barrier();
 
-        std::vector<sa_index_t> sa(mgpus[world_rank()].working_len);
-        cudaMemcpy(sa.data(), mgpus[world_rank()].Sa_rank, sizeof(sa_index_t) * mgpus[world_rank()].working_len, cudaMemcpyDeviceToHost);
-
-        auto check = comm_world().gatherv(send_buf(sa), root(0));
-        comm_world().barrier();
-
-        if (world_rank() == 0) {
-            size_t current_rank = 1;
-            size_t rank_buffer = 0;
-            for (size_t i = 1; i < check.size(); i++)
-            {
-                if (check[i] == check[i - 1]) {
-                    if (allKmer[i] != allKmer[i - 1]) {
-                        printf("%lu and %lu are not equal but have the same rank\n", i - 1, i);
-                    }
-                    ASSERT(allKmer[i] == allKmer[i - 1]);
-                    rank_buffer++;
-                }
-                else {
-                    if (current_rank + rank_buffer + 1 != check[i]) {
-                        printf("[%lu] current rank: %lu + rank_buffer: %lu + 1 != next rank %u", i, current_rank, rank_buffer, check[i]);
-                    }
-                    ASSERT(current_rank + rank_buffer + 1 == check[i]);
-                    rank_buffer = 0;
-                    current_rank = check[i];
-                }
-            }
-        }
-        comm_world().barrier();
+        // if (world_rank() == 0) {
+        //     size_t current_rank = 1;
+        //     size_t rank_buffer = 0;
+        //     for (size_t i = 1; i < check.size(); i++)
+        //     {
+        //         if (check[i] == check[i - 1]) {
+        //             if (allKmer[i] != allKmer[i - 1]) {
+        //                 printf("%lu and %lu are not equal but have the same rank\n", i - 1, i);
+        //             }
+        //             ASSERT(allKmer[i] == allKmer[i - 1]);
+        //             rank_buffer++;
+        //         }
+        //         else {
+        //             if (current_rank + rank_buffer + 1 != check[i]) {
+        //                 printf("[%lu] current rank: %lu + rank_buffer: %lu + 1 != next rank %u", i, current_rank, rank_buffer, check[i]);
+        //             }
+        //             ASSERT(current_rank + rank_buffer + 1 == check[i]);
+        //             rank_buffer = 0;
+        //             current_rank = check[i];
+        //         }
+        //     }
+        // }
+        // comm_world().barrier();
         // for (uint gpu_index = 0; gpu_index < NUM_GPUS; ++gpu_index)
         // {
 
@@ -971,13 +965,11 @@ private:
                     // uses: Temp3, 4
                     sa_index_t* input_buffer = gpu.Temp1;
                     sa_index_t* temp_buffer = gpu.Temp3;
-                    sa_index_t* add_buffer;
+
                     if (initial) {
                         input_buffer = reinterpret_cast<sa_index_t*>(in_buffer[gpu_index] ? gpu.Kmer : gpu.Kmer_buffer);
-
-                        cudaMalloc(&add_buffer, sizeof(sa_index_t) * gpu.working_len);
-                        out_buffer = add_buffer;//in_buffer[gpu_index] ? gpu.Sa_rank : gpu.Sa_index;
-                        // temp_buffer = in_buffer[gpu_index] ? gpu.Temp3 : gpu.Kmer_temp1;
+                        out_buffer = in_buffer[gpu_index] ? gpu.Sa_rank : reinterpret_cast<sa_index_t*>(gpu.Kmer);
+                        temp_buffer = in_buffer[gpu_index] ? gpu.Temp3 : gpu.Kmer_temp1;
                     }
 
                     MaxFunctor max_op;
@@ -990,9 +982,8 @@ private:
                     CUERR_CHECK(err);
                     // printf("[%lu] after prep\n", world_rank());
                     // Run inclusive prefix max-scan
-                    void* temp;
-                    cudaMalloc(&temp, temp_storage_bytes);
-
+                    // void* temp;
+                    // cudaMalloc(&temp, temp_storage_bytes);
                     if (in_buffer[gpu_index] || !initial) {
                         ASSERT(temp_storage_bytes < 2 * mreserved_len * sizeof(sa_index_t) + madditional_temp_storage_size);
                     }
@@ -1000,13 +991,12 @@ private:
                         ASSERT(temp_storage_bytes <= mmemory_manager.get_temp_mem_kmer());
                     }
 
-                    err = cub::DeviceScan::InclusiveScan(temp, temp_storage_bytes, input_buffer, out_buffer,
+                    err = cub::DeviceScan::InclusiveScan(temp_buffer, temp_storage_bytes, input_buffer, out_buffer,
                         max_op, gpu.working_len, mcontext.get_gpu_default_stream(gpu_index));
                     cudaMemcpyAsync(mhost_temp_mem + gpu_index, out_buffer + gpu.working_len - 1,
                         sizeof(sa_index_t), cudaMemcpyDeviceToHost,
                         mcontext.get_gpu_default_stream(gpu_index));
                     CUERR;
-                    cudaFreeAsync(temp, mcontext.get_gpu_default_stream(gpu_index));
 
                 }
                 // Now temp1 is written to Sa_rank
@@ -1042,11 +1032,9 @@ private:
                 //(mcontext.get_device_id(gpu_index));
                 kernels::write_if_eq _KLC_SIMPLE_(gpu.working_len, mcontext.get_gpu_default_stream(gpu_index))(out_buffer, gpu.Sa_rank, 0, mhost_temp_mem[gpu_index - 1], gpu.working_len);
                 CUERR;
-                if (initial) {
-                    cudaFreeAsync(out_buffer, mcontext.get_gpu_default_stream(gpu_index));
-                }
             }
         }
+
         mcontext.sync_default_streams();
         // printArrayss << <1, 1 >> > (mgpus[world_rank()].Sa_rank, std::min(20UL, mgpus[world_rank()].working_len), world_rank());
         // mcontext.sync_default_streams();
